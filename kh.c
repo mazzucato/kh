4f0c37b5ef133e5677238d3a155b866b4321d8e3
mazzucato <victor.philippe@hotmail.com>
2018-07-05 16:09:18 -0300

    Create kh.c

---
+++ kh.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+
+    int entrada,aux;
+    char kh[2] = "&#128169;";
+    printf("%s\n",kh);
+return 0;
+
+}
